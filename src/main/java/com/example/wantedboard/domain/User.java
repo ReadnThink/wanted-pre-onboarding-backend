235e6cf69dd5679bd3b13fb19d2caea9dac57000
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-07 16:29:32 +0900

    User Entity Setting

---
+++ src/main/java/com/example/wantedboard/domain/User.java
@@ -0,0 +1,38 @@
+package com.example.wantedboard.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Post> posts;
+
+    @Builder
+    public User(final Long id, final String name, final String email, final String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+}
+
